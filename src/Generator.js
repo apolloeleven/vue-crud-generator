9e4db61d418933fbed5191722d36757321e0f377
Saiat Kalbiev <kalbievich11@gmail.com>
2019-08-15 12:46:11 +0400

    Finish main component template generation logic.

--- src/Generator.js
+++ src/Generator.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const path = require('path');
+const pluralize = require('pluralize');
+
 const GeneralHelper = require('./helpers/general-helper');
 
 class Generator {
@@ -28,10 +30,12 @@ class Generator {
 
   createMainComponent() {
 
-    let mainDivClass = this.name.toLowerCase();
+    let mainDivClass = GeneralHelper.unCapitalize(this.name);
+    let mainUrl = mainDivClass.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`)
     let componentName = GeneralHelper.capitalize(mainDivClass);
+    let serviceName = `${componentName}Service`;
 
-    const templateFile = path.join(__dirname, 'templates/main.component.vue');
+    const templateFile = path.join(__dirname, 'templates/main.component');
     const newFile = path.join(this.path, `${componentName}.vue`);
 
     try {
@@ -40,18 +44,33 @@ class Generator {
       throw new Error(`Error occurred while copying main component template file. Message: ${err.message}`);
     }
 
-    fs.readFile(newFile, 'utf8', function (err, data) {
+    fs.readFile(newFile, 'utf8', (err, data) => {
       if (err) throw new Error(`Error while reading newly created file. Message: ${err.message}`);
 
       let result = data
         .replace('{{mainDivClass}}', mainDivClass)
-        .replace('{{componentName}}', componentName);
+        .replace(/{{componentName}}/g, pluralize.plural(componentName))
+        .replace(/{{listVariableName}}/g, pluralize.plural(mainDivClass))
+        .replace('{{tableFields}}', this.getTableFieldsFileData())
+        .replace(/{{mainUrl}}/g, mainUrl)
+        .replace(/{{serviceName}}/g, serviceName);
 
       fs.writeFile(newFile, result, 'utf8', function (err) {
         if (err) throw new Error(`Error while saving newly updated file. Message: ${err.message}`)
       });
     });
 
+  }
+
+  getTableFieldsFileData() {
+    let returnData = "";
+    for (var i = 0; i < this.field_names.length; i++) {
+      returnData += `\t\t\t\t\t'${this.field_names[i]}',`;
+      if (i !== this.field_names.length - 1) {
+        returnData += '\n';
+      }
+    }
+    return returnData;
   }
 
 }
