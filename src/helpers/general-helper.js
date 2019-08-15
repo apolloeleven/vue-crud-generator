5be1015e8ab522a9890253cc98386b24390bd277
Saiat Kalbiev <kalbievich11@gmail.com>
2019-08-15 12:42:38 +0400

    Add unCapitalize method to general helper class

--- src/helpers/general-helper.js
+++ src/helpers/general-helper.js
@@ -5,5 +5,12 @@ module.exports = {
     } else {
       return s.charAt(0).toUpperCase() + s.slice(1);
     }
-  }
+  },
+  unCapitalize: (s) => {
+    if (typeof s !== 'string') {
+      return ''
+    } else {
+      return s.charAt(0).toLowerCase() + s.slice(1);
+    }
+  },
 };
