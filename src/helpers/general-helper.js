b189caab9143c043cff77f56cea5bc4ed6586112
Saiat Kalbiev <kalbievich11@gmail.com>
2019-08-15 14:46:48 +0400

    Add emptyDir method to general helper class

--- src/helpers/general-helper.js
+++ src/helpers/general-helper.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = {
   capitalize: (s) => {
     if (typeof s !== 'string') {
@@ -13,4 +16,15 @@ module.exports = {
       return s.charAt(0).toLowerCase() + s.slice(1);
     }
   },
+  emptyDir: (dir) => {
+    fs.readdir(dir, (err, files) => {
+      if (err) throw err;
+
+      for (const file of files) {
+        fs.unlink(path.join(dir, file), err => {
+          if (err) throw err;
+        });
+      }
+    });
+  }
 };
