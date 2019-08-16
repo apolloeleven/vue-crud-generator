053b9e87f6f0aa5e2622eddf2db04604ad56828b
Saiat Kalbiev <kalbievich11@gmail.com>
2019-08-16 17:14:10 +0400

    Add eachWordCapitalize helper method to general helper

--- src/helpers/general-helper.js
+++ src/helpers/general-helper.js
@@ -1,30 +1,45 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = {
-  capitalize: (s) => {
-    if (typeof s !== 'string') {
-      return ''
-    } else {
-      return s.charAt(0).toUpperCase() + s.slice(1);
-    }
-  },
-  unCapitalize: (s) => {
-    if (typeof s !== 'string') {
-      return ''
-    } else {
-      return s.charAt(0).toLowerCase() + s.slice(1);
-    }
-  },
-  emptyDir: (dir) => {
-    fs.readdir(dir, (err, files) => {
-      if (err) throw err;
-
-      for (const file of files) {
-        fs.unlink(path.join(dir, file), err => {
-          if (err) throw err;
-        });
-      }
-    });
+capitalize = (s) => {
+  if (typeof s !== 'string') {
+    return ''
+  } else {
+    return s.charAt(0).toUpperCase() + s.slice(1);
   }
+};
+
+unCapitalize = (s) => {
+  if (typeof s !== 'string') {
+    return ''
+  } else {
+    return s.charAt(0).toLowerCase() + s.slice(1);
+  }
+};
+
+eachWordCapitalize = (s) => {
+  s = s.split(" ");
+  for (var i = 0, x = s.length; i < x; i++) {
+    s[i] = s[i][0].toUpperCase() + s[i].substr(1);
+  }
+  return s.join(" ");
+};
+
+emptyDir = (dir) => {
+  fs.readdir(dir, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      fs.unlink(path.join(dir, file), err => {
+        if (err) throw err;
+      });
+    }
+  });
+};
+
+module.exports = {
+  capitalize,
+  unCapitalize,
+  eachWordCapitalize,
+  emptyDir
 };
